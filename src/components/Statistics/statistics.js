5f2d57ef4bad150d989b1b40217da1f2a8b5580f
Hannah Huang <yunhanh@bu.edu>
2020-06-17 16:40:08 -0400

    Clean gh-pages

--- src/components/Statistics/statistics.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Statistics = () => (
-  <div>
-    <h1>2020 TTB Statistics</h1>
-    <div className="bar-sm"></div>
-  </div>
-);
-export default Statistics;
--- src/components/sponsors/sponsor.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Sponsor = () => (
-  <div>
-    <h1>Our sponsors this year</h1>
-    <div className="bar-sm"></div>
-  </div>
-);
-
-export default Sponsor;
